aca8a5263bb9629ed275dd24127f67825a83fc1a
a-gevorgAL <gevorgarghushyan8@gmail.com>
2024-12-16 20:08:03 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+const   App = () => {
+  return (
+    <div className="App">
+      <Header/>
+      <br/>
+      simple html
+ 
+    </div>
+  );
+}
+
+
+const   Header = () => {
+  return (
+    <div className="header">
+      header html
+    </div>
+  );
+}
+
+export default App;
